25c1fbf03b0a3c7d8fbef7a6c2cc849aa4aa358c
Gabriel Raposo <gabriel.robertor@gmail.com>
2022-05-23 19:44:49 -0300

    Initial Commit

---
+++ Timer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Timer
+//
+//  Created by Gabriel Raposo on 5/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Gabriel Raposo on 5/23/22.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
